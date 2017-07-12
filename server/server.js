8d3a71cf786f207b94af27b10421698e5eaf5f3a
Rafael Vicio <rafaelvicio@icloud.com>
2017-07-11 23:34:52 -0300

    Projeto - aula2

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var http = require('http')
     ,app = require('./config/express')
     ,db = require('./config/database');
 
-app.listen(3001, function() {
+app.listen(3000, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
