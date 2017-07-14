c027bbbdc12bf26f1f5fb3cd3584a7bc86a6cb61
Rafael Vicio <rafaelvicio@icloud.com>
2017-07-13 21:46:44 -0300

    Aula quinta

--- caelumpic/src/app/listagem/listagem.component.ts
+++ caelumpic/src/app/listagem/listagem.component.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FotoService } from '../foto/foto.service';
+
 @Component({
   selector: 'listagem',
   templateUrl: './listagem.component.html'
 })
+
 export class ListagemComponent {
+
   fotos: FotoComponent[] = [];
+  service: FotoService;
+  mensagem = '';
+
   constructor(service: FotoService) {
-    service.lista()
+
+    this.service = service;
+
+    this.service.lista()
       .subscribe(
       fotos => this.fotos = fotos,
       erro => console.log(erro)
       );
+
   }
+
+  remove(foto) {
+
+    this.service
+      .remove(foto)
+      .subscribe(
+        () => {
+          let novasFotos = this.fotos.slice(0);
+          let indice = this.fotos.indexOf(foto);
+          this.fotos.splice(indice, 1);
+          this.fotos = novasFotos;
+          this.mensagem = 'Foto removida com sucesso!';
+        },
+        erro => {
+          console.log(erro)
+          this.mensagem = 'Erro ao remover foto';
+        }
+      )
+
+  }
+
 }
